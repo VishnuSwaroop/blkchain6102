4e8660cb0d67604514a022196c0c10f10b4835c3
VishnuSwaroop <vish1018@gmail.com>
2016-04-14 12:38:19 -0400

    4096 RSA

--- jsonGen.py
+++ jsonGen.py
@@ -6,12 +6,12 @@ from Crypto import Random
 listofkeys=[]
 for i in range(10):
     random_generator = Random.new().read
-    key1 = RSA.generate(1024, random_generator)
+    key1 = RSA.generate(4096, random_generator)
     listofkeys.append(key1)
 
 #<_RSAobj @0x7f60cf1b57e8 n(1024),e,d,p,q,u,private>
 random_generator = Random.new().read
-cnds_key = RSA.generate(1024, random_generator)
+cnds_key = RSA.generate(4096, random_generator)
 CNDSpvtkey=(cnds_key.publickey().n,cnds_key.publickey().e,cnds_key.d)
 #print(type(nodepvtkey))
 CNDSpubkey=(cnds_key.publickey().n,cnds_key.publickey().e)
