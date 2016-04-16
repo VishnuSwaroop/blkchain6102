8a45dc3c7e739873d968333ccfc5602e4e757c3a
macchiad <macchiad@live.com>
2016-04-16 18:24:54 -0400

    Added transaction validation node class

--- NodeTest.py
+++ NodeTest.py
@@ -1,44 +1,5 @@
 import sys
-from NodeServer import NodeServer
-from NodeClient import NodeClient
-from twisted.internet import reactor
-
-class TxValidateNode:
-    def __init__(self, cndsIp, cndsPort, localIp, localPort):
-        self.reactor = reactor
-        self.cnds = NodeClient(self.reactor, cndsIp, cndsPort)
-        self.cnds.onConnect = self.cndsOnConnect
-        self.cnds.onReceive = self.cndsDataReceived
-        
-        self.server = NodeServer(self.reactor, localIp, localPort, 10)
-        self.server.onConnect = self.nodeOnConnect
-        self.server.onReceive = self.nodeOnReceive
-        
-        self.pingCount = 0
-        
-    def joinRequest(self):
-        return "join"
-        
-    def cndsOnConnect(self, node, addr):
-        return self.joinRequest()
-        
-    def cndsDataReceived(self, addr, data):
-        if data == "approved":
-            print("Join succeeded")
-        elif data == "hello?":
-            print("Pinged by CNDS")
-            self.pingCount += 1
-            return "hello"
-        else:
-            print("Join failed")
-            
-        return None
-
-    def nodeOnConnect(self, node, addr):
-        print("Connected to node at " + addrToStr(addr))
-            
-    def nodeOnReceive(self, addr, data):
-        print("Received from node at " + addrToStr(addr) + ": " + data)
+from TxValidateNode import TxValidateNode
 
 def main(args):
     cndsIp = "localhost"
@@ -55,7 +16,6 @@ def main(args):
 
     print("Starting NodeTest")
     node = TxValidateNode(cndsIp, cndsPort, localIp, localPort)
-    reactor.run()
     
 if __name__ == "__main__":
     main(sys.argv)
