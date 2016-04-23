a9dc85f9dbe70c7dbed85e45e555fada2c64b36e
VishnuSwaroop <vish1018@gmail.com>
2016-04-23 19:56:38 -0400

    combined code

--- NodeTest.py
+++ NodeTest.py
@@ -1,5 +1,47 @@
 import sys
-from TxValidateNode import TxValidateNode
+from node_structure import *
+from NodeServer import NodeServer
+from NodeClient import NodeClient
+from twisted.internet import reactor
+
+class TxValidateNode(node_methods):
+    def __init__(self, cndsIp, cndsPort, localIp, localPort):
+        self.reactor = reactor
+        
+        self.cnds = NodeClient(self.reactor, cndsIp, cndsPort)
+        self.cnds.onConnect = self.cndsOnConnect
+        self.cnds.onReceive = self.cndsDataReceived
+        
+        self.server = NodeServer(self.reactor, localIp, localPort, 10)
+        self.server.onConnect = self.nodeOnConnect
+        self.server.onReceive = self.nodeOnReceive
+        
+        self.pingCount = 0
+        
+    def run(self):
+        self.reactor.run()
+        
+    def cndsOnConnect(self, node, addr):
+        return self.join_req()
+        
+    def cndsDataReceived(self, addr, data):
+        msg = self.
+        if data == "approved":
+            self.connection_status(True)
+        elif data == "hello?":
+            print("Pinged by CNDS")
+            self.pingCount += 1
+            return "hello"
+        else:
+            print("Join failed")
+            
+        return None
+
+    def nodeOnConnect(self, node, addr):
+        print("Connected to node at " + addrToStr(addr))
+            
+    def nodeOnReceive(self, addr, data):
+        print("Received from node at " + addrToStr(addr) + ": " + data)
 
 def main(args):
     cndsIp = "localhost"
@@ -13,9 +55,11 @@ def main(args):
         cndsPort = int(args[2])
         localIp = args[3]
         localPort = int(args[4])
-
+    
     print("Starting NodeTest")
     node = TxValidateNode(cndsIp, cndsPort, localIp, localPort)
+    node.createnode(localIp, cndsIp)
+    node.run()
     
 if __name__ == "__main__":
     main(sys.argv)
