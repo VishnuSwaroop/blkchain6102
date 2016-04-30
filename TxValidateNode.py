e82e8368c4535e57d34b092b155e525c922e80cf
VishnuSwaroop <vish1018@gmail.com>
2016-04-29 23:51:57 -0400

    blockchain implemented

--- TxValidateNode.py
+++ TxValidateNode.py
@@ -3,6 +3,11 @@ from twisted.web import server, resource
 from twisted.internet import reactor, endpoints
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from TxNodeUtil import *
+from tx_format import *
+import json
+#from  import *
+from blk_format import *
+from hashcash import *
 
 class TxValidateNode(resource.Resource):
     isLeaf = True
@@ -87,6 +92,248 @@ class TxValidateNode(resource.Resource):
         
     def render_POST(self, request):
         return self.render_response(request, self.handle_post)
+
+    
+    
+    
+    
+    
+    #-----------------------------------------------------------------------------------VISHNU
+    #Storing node info locally
+    def store_network_info(self,network_info): #store response to nodeinfo request
+        with open('network.json', 'w') as outfile:
+            json.dump(network_info, outfile, indent=4, sort_keys=True)
+    
+    
+    #Starting Blokchain Procedures
+    #receive transactions from the originating node and store them till they overflow 
+    def receive_new_tx(self, transaction): #transaction ={prev tx hash+asset data+current tx hash}
+        # tx object ===> (self, origin, owner,value, owner_pubkey,upper_limit, previous_hash=None, current_hash=None):
+        
+        new_tx=tx_object(transaction['origin'], transaction['owner'],transaction['value'], transaction['owner_pubkey'],transaction['upper_limit'], transaction['previous_hash'], transaction['current_hash'])
+        new_tx.to_string()
+        
+        with open('tx_database.json','r') as data_file:    
+            dict_state = json.load(data_file)
+        #dict_state[transaction['current_hash']]= {'previous_hash':transaction['previous_hash'],'current_dict':new_tx.to_dict()}
+        
+        dict_state[transaction['current_hash']]= new_tx.to_dict()  #Here the previous hash inside new_tx could be None or some dict, if None, it is a new origin node that is sending a tx
+        
+        with open('tx_database.json', 'w') as outfile:
+            json.dump(dict_state, outfile, indent=4, sort_keys=True) #adding to unconfirmed pool
+        
+    
+    def receive_tx_broadcast(self,transaction,new_node_info):#receive transaction broadcast and node information from a node contacting other for the first time
+        new_tx=tx_object(transaction['origin'], transaction['owner'],transaction['value'], transaction['owner_pubkey'],transaction['upper_limit'], transaction['previous_hash'], transaction['current_hash'])
+        new_tx.to_string()
+        
+        with open('tx_database.json','r') as data_file:    
+            dict_state = json.load(data_file)
+        #dict_state[transaction['current_hash']]= {'previous_hash':transaction['previous_hash'],'current_dict':new_tx.to_dict()}
+        
+        dict_state[transaction['current_hash']]= new_tx.to_dict()  #Here the previous hash inside new_tx could be None or some dict, if None, it is a new origin node that is sending a tx
+        
+        with open('tx_database.json', 'w') as outfile:
+            json.dump(dict_state, outfile, indent=4, sort_keys=True) #adding to unconfirmed pool
+        
+        
+        with open('network.json','r') as data_file:    
+            dict_state = json.load(data_file)
+       
+        dict_state[new_node_info['nodename']]={'nodeip':new_node_info['nodeip'],'nodepubkey':new_node_info['nodepubkey']}   
+        
+        with open('network.json', 'w') as outfile:
+            json.dump(dict_state, outfile, indent=4, sort_keys=True)#store the information about the new node
+            
+    
+    def check_overflow(self): #returns True if overflow, otherwise returns false
+        with open('tx_database.json','r') as data_file:    
+            dict_state = json.load(data_file)
+        
+       
+        if len(dict_state.keys()) >= 8:
+            print '########## Transaction buffer full ###########'    
+            # block=self.generate_block(dict_state)
+            # block.toString()
+            # with open('tx_database.json','w') as outfile:    
+            #     json.dump('', outfile, indent=4, sort_keys=True) #clearing unconfirmed pool 
+            # return block #this is an object, not a dict. Should we convert ?
+            return dict_state
+        else:
+            return False
+        
+    def generate_block(self,dict_state): #dict_state is a dict of transactions
+        print '########## Generating Block ###########'   
+        
+        with open('block_config.json','r') as data_file:    
+            block_config = json.load(data_file)
+        # with open('tx_database.json','r') as data_file:    
+        #     dict_state = json.load(data_file)
+        with open('newest_blkhash.json','r') as data_file: #reads the hash of the latest added block
+            newest_hash_dict=json.load(data_file)
+        prev_hash=newest_hash_dict['newest_hash']
+        
+        if prev_hash=='':
+            prev_hash=None #check for error here
+        
+        block=block_object(block_config,dict_state,prev_hash)
+        block.toString()
+        return block #returns block with all info except nonce
+    
+    
+    
+    def hashcash_start(self,block): #gets the output of generate_block as input
+        with open('tx_database.json','w') as outfile:    
+            json.dump('', outfile, indent=4, sort_keys=True) #clearing unconfirmed pool 
+        
+
+        block_dict=block.to_dict()
+        prev_hash=block_dict['block_header']['previoushash']
+        header_hash=block_dict['magicnum'] +block_dict['block_header']['version']+ block_dict['block_header']['merklehash']+block_dict['block_header']['time']+block_dict['txcount']
+        # difficulty=int(blockchain_state['prev_hash']['header']['difficulty']) #either add 1 to the latest difficulty value, or accept some global value)
+        difficulty=12
+        
+        nonce_return=HashCash.generate(prev_hash,header_hash,difficulty) #might get stopped in the middle
+        
+        
+        
+        #Maybe move this ?????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
+        block_dict['block_header']['nonce']=nonce_return
+        
+        with open('blockchain_database.json','r') as data_file:    
+            blockchain_state = json.load(data_file) #returns the entire blockchain        
+        
+        #should we include previous hash here ? Will that work with the nonce while verification ? because its only used for comparison in the generation of nonce
+        hashdata=block_dict['magicnum'] +block_dict['block_header']['version']+ block_dict['block_header']['merklehash']+block_dict['block_header']['time']+block_dict['txcount']+str( block_dict['block_header']['nonce'])
+        block_hash=SHA512.new(bytes(hashdata)).digest()
+        
+        
+        blockchain_state[block_hash]=block_dict
+        
+        with open('blockchain_database.json','w') as outputfile:    
+            json.dump(blockchain_state,outputfile,indent=4, sort_keys=True) #writes newly generated block to block chain  
+        
+        with open('newest_blkhash.json','w') as data_file: #stores the hash of the latest added block
+            json.dump({'newest_hash':block_hash},data_file,indent=4, sort_keys=True)
+            
+        
+        #send block to broadcast
+        block_broadcast={'block_hash':block_hash,'block_dict':block_dict}
+            
+            
+
+    def validate_block(self, block_broadcast): # Done on receiving a block from another node (not on getting every new tx). This might take long. 
+
+        with open('blockchain_database.json','r') as data_file:    
+            blockchain_dict = json.load(data_file,object_pairs_hook=OrderedDict ) #returns the entire blockchain, with the ordered dict
+
+        tx_order_dict=blockchain_dict['transactions']
+        flag=False
+        root_verify= self.set_merkle(tx_order_dict)
+        if root_verify==str(block_broadcast['block_dict']['block_header']['merklehash']):
+            print('========================= Merkle root for block %s is Valid !==============================' % (block_broadcast['block_hash']))
+            flag=True
+        else:
+            print('========================= Merkle root for block %s is NOT Valid !==============================' % (block_broadcast['block_hash']))
+            return False
+            
+        
+        
+        if(flag==True):
+            with open('newest_blkhash.json','r') as data_file: #reads the hash of the latest added block
+                    newest_hash_dict=json.load(data_file)
+            newest_blkhash=newest_hash_dict['newest_hash']
+            #check hash of latest block
+            if block_broadcast['block_dict']['block_header']['previoushash']==newest_blkhash:
+                tx_verify=''
+                for tx in tx_order_dict.keys():
+                    blockhash=newest_blkhash
+                    while blockhash != None:
+                        #value = d.get(key, "empty")
+                        tx_verify=blockchain_dict[blockhash]['transactions'].get(tx['previous_hash'])
+                        if tx_verify not in ('', None):
+                            print ('------------------ Transaction Verified !! ----------------------')
+                            break
+                        else:
+                            blockhash=blockchain_dict[blockhash]['block_header']['previoushash']
+                
+                    
+                    
+                    if tx_verify in ('',None):
+                        print ('------------------ A transaction is not verified !! ----------------------')
+                        return False
+                
+                
+                #If all transactions valid, store block . Reuse above code to add to file
+        
+                with open('blockchain_database.json','r') as data_file:    
+                    blockchain_state = json.load(data_file) #returns the entire blockchain
+                
+                
+                blockchain_state[block_broadcast['block_hash']]=block_broadcast['block_dict']
+                
+                with open('blockchain_database.json','w') as outputfile:    
+                    json.dump(blockchain_state,outputfile,indent=4, sort_keys=True) #writes newly generated block to block chain  
+                
+                with open('newest_blkhash.json','w') as data_file: #stores the hash of the latest added block
+                    json.dump({'newest_hash':block_hash},data_file,indent=4, sort_keys=True)
+            
+                
+                
+            else:
+                print('Previous block not in blockchain !! ')
+            
+            
+            
+            
+            
+    
+    def set_merkle(self,tx_order_dict):
+        #tx_order_dict= OrderedDict(tx_dict)
+        hashes1=[]
+        hashes2=[]
+        hashes3=[]
+        
+        i=0
+        while (i< len(tx_order_dict.keys())):#every key is a hash of the tx, range=8
+            temp=tx_order_dict.keys()[i]+tx_order_dict.keys()[i+1]
+            i=i+2
+            hashes1.append(SHA512.new(bytes(temp)).digest()) #has 4 hashes
+        i=0
+        while (i< len(hashes1)):#every key is a hash of the tx, range=4
+            temp=hashes1[i]+hashes1[i+1]
+            i=i+2
+            hashes2.append(SHA512.new(bytes(temp)).digest()) #has 2 hashes
+        i=0
+        while (i< len(hashes2)):#every key is a hash of the tx, range=2
+            temp=hashes2[i]+hashes2[i+1]
+            i=i+2
+            root_hash=SHA512.new(bytes(temp)).digest() #has the root hash
+            print ('Merkle root calculated :')
+            print root_hash
+            
+        return root_hash
+        
+
+    def merge_unconfirmedpool(self,pool1,pool2,pool3):
+        with open('tx_database.json','r') as data_file:    
+            pool_state = json.load(data_file)
+        
+        
+        pool1_keys=pool1.keys()
+        pool2_keys=pool2.keys()
+        pool3_keys=pool3.keys()
+        
+        common=((pool1_keys | pool2_keys) & pool3_keys)
+        common=list(common)
+        final=pool1.copy()
+        final.update(pool2) #does the union
+        # del c['b']
+        for i in common:
+            del final[i] #delete the common elements
+            
+        return final
+
 
 def main(args):
     cnds_ip = "localhost"
---
+++ blk_format.py
@@ -0,0 +1,104 @@
+from Crypto.Hash import SHA512
+from filetostring import *
+from collections import *
+
+      
+class block_header:
+    def __init__(self, blockconfig,merkle,prev_hash): #blockconfig is a dictionary or a json file
+        self.version = blockconfig['version']
+        self.previoushash = prev_hash
+        self.merklehash = merkle
+        self.time = None
+        self.bits = blockconfig['bits']
+        self.nonce = None
+    
+    def toString(self):
+        print "Version: %d" % self.version
+        print "Previous Hash %s" % hashStr(self.previoushash)
+        print "Merkle Root %s" % hashStr(self.merklehash)
+        print "Time %s" % str(self.time)
+        print "Difficulty %8x" % self.bits
+        print "Nonce %s" % self.nonce
+        
+    def to_dict(self):
+        dict_form={'version':self.verison,
+                   'previoushash':self.previoushash,
+                   'merklehash':self.merklehash,
+                   'time':self.time,
+                   'bits':self.bits,
+                   'nonce':self.nonce}
+        return dict_form
+        
+        
+class block_object:
+    def __init__(self, blockconfig, tx_dict,prev_hash):
+        
+        tx_order_dict= OrderedDict(tx_dict)
+        self.magicnum = (blockconfig['magicnum'])
+        self.blocksize = None
+        self.setHeader(blockconfig,tx_order_dict,prev_hash)
+        self.txcount = len(tx_order_dict.keys()) #fixed no of transactions=8
+        self.transactions=tx_order_dict
+    
+    def setHeader(self, blockconfig,tx_order_dict):
+        merkle=self.set_merkle(tx_order_dict)
+        self.blockHeader = block_header(blockconfig,merkle,prev_hash)
+                
+    
+    def set_merkle(self,tx_order_dict):
+
+        hashes1=[]
+        hashes2=[]
+        hashes3=[]
+        
+        i=0
+        while (i< len(tx_order_dict.keys())):#every key is a hash of the tx, range=8
+            temp=tx_order_dict.keys()[i]+tx_order_dict.keys()[i+1]
+            i=i+2
+            hashes1.append(SHA512.new(bytes(temp)).digest()) #has 4 hashes
+        i=0
+        while (i< len(hashes1)):#every key is a hash of the tx, range=4
+            temp=hashes1[i]+hashes1[i+1]
+            i=i+2
+            hashes2.append(SHA512.new(bytes(temp)).digest()) #has 2 hashes
+        i=0
+        while (i< len(hashes2)):#every key is a hash of the tx, range=2
+            temp=hashes2[i]+hashes2[i+1]
+            i=i+2
+            root_hash=SHA512.new(bytes(temp)).digest() #has the root hash
+            print ('Merkle root calculated :')
+            print root_hash
+            
+        return root_hash
+        
+        
+            
+    
+    def toString(self):
+        print ""
+        print "Magic No: ",self.magicnum
+        print "Blocksize: \t", self.blocksize
+        print ""
+        print "#"*10 + " Block Header " + "#"*10
+        self.blockHeader.toString()
+        print
+        print "##### Tx Count: %d" % self.txCount
+        i=0
+        for t in self.transactions:
+            print ('Transaction %d'% i)
+            i=i+1
+            t.to_string()
+            
+    def to_dict(self):
+        #         self.magicnum = (blockconfig['magicnum'])
+        # self.blocksize = (blockconfig['blocksize'])
+        # self.setHeader(blockconfig,tx_dict)
+        # self.txcount = (8) #fixed no of transactions
+        # self.transactions=tx_dict
+    
+        dict_form={'magicnum':self.magicnum,
+                   'blocksize':self.blocksize,
+                   'block_header':block_header.to_dict,
+                   'txcount':self.txcount,
+                   'transactions':self.transactions}
+        return dict_form
